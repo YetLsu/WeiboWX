6bb4a794937ca6309f069f8d1cba6eca6cf5c182
玩火的萌娃 <wanhuodemengwa@wanhuodemengwade.local>
2015-10-14 10:48:18 +0800

    Initial Commit

---
+++ wh_weibo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  wh_weibo
+//
+//  Created by 玩火的萌娃 on 15/10/14.
+//  Copyright (c) 2015年 玩火的萌娃. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
